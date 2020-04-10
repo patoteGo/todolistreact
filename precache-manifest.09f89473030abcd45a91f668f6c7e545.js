94db65467d57246b5a6c1383f2e2edcee48d44ac
patodekaz <pato@dekaz.cl>
2020-04-10 18:15:59 -0400

    Updates

---
+++ precache-manifest.09f89473030abcd45a91f668f6c7e545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a8221f03f1d55500d9df313853f579",
+    "url": "/todolistreact/index.html"
+  },
+  {
+    "revision": "335678e7600d54f5b3e4",
+    "url": "/todolistreact/static/css/main.cbac2cf3.chunk.css"
+  },
+  {
+    "revision": "7155df12ec45bb19cef8",
+    "url": "/todolistreact/static/js/2.5fb14807.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/todolistreact/static/js/2.5fb14807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "335678e7600d54f5b3e4",
+    "url": "/todolistreact/static/js/main.535c2495.chunk.js"
+  },
+  {
+    "revision": "c0be5a8c689589f2ebbe",
+    "url": "/todolistreact/static/js/runtime-main.a9834dae.js"
+  }
+]);
